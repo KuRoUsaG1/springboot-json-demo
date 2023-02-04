5c6392151b1eb67f121506df02a78480f7c4124a
kurousagi <allen.liuyu@gmail.com>
2023-02-03 23:31:43 -0800

    using the @Value annotation to map the endpoint with url link.

--- src/main/java/com/example/springbootjsonnew/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootjsonnew/service/Impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.springbootjsonnew.entity.User;
 import com.example.springbootjsonnew.service.UserService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,6 +15,9 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Value("${end-point}")
+    private String endPoint;
+
     @Override
     public List<UserInfo> getUserInfo() {
         List<User> users = retrieveUsers();
@@ -34,7 +38,7 @@ public class UserServiceImpl implements UserService {
         List<User> users = new ArrayList<>();
         try {
             ObjectMapper mapper = new ObjectMapper();
-            URL url = new URL("https://jsonplaceholder.typicode.com/users");
+            URL url = new URL(endPoint);
             users= mapper.readValue(url, new TypeReference<List<User>>() {});
         } catch (IOException e) {
             System.out.println(e);
